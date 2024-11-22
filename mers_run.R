9fb9ea92057c7780ed68ee131f01ea4f2f45be54
peter yang <pyangmain@gmail.com>
2024-11-22 12:25:06 -0500

    modified nexus tree

--- mers_run.R
+++ mers_run.R
@@ -9,10 +9,24 @@ setwd('/home/pyang/projects/yang-phylopomp')
 
 treeio::read.nexus("MERS_274_sCoal.combinedTyped.mcc.tree") -> x
 
+
+x$root.edge <- 1 # edit root edge
 x$tip.label[] <- "b_"
 x$node.label[] <- "g_"
 
-x |> plot()
+plot(x, show.node.label = TRUE, root.edge = TRUE)
+
+# It seems that write.tree() returns a newick object, and from what i've seen online,
+# Newick trees don't support root edges. Not sure how to proceed here...
+# Probably need to find a way to edit the raw gpgen in mers_pomp...
+
+# TODO: if more than one thing coming out of a root, insert the appropriate number of roots
+# Wrap whole thing in parenthesis
+#x |>
+#  treeio::write.tree() |>
+#  parse_newick() |>
+#  curtail(time=2, prune=FALSE) |>
+#  diagram(prune=FALSE)
 
 x |>
   treeio::write.tree() |>
@@ -20,35 +34,48 @@ x |>
   plot(points=TRUE,ladderize=FALSE)
 
 mers_pomp <- parse_newick(treeio::write.tree(x))
+plot(mers_pomp, 5, 0)
+
+#diagram(curtail(mers_pomp,0.5),prune=F,obscure=F)
 
 pomp_obj <- twospecies_pomp(
-  mers_pomp, # Parameters taken from example TODO: change
-  c1=0.5,c2=0.5,
-  Beta11=4,Beta12=1,
-  Beta21=1,Beta22=4,
-  gamma1=1,gamma2=1,
-  psi1=1,psi2=1,
-  S1_0=100,I1_0=2,R1_0=10,
-  S2_0=100,I2_0=2,R2_0=10,
-  b1=0.1,b2=0.1,
-  d1=0.1,d2=0.1,
-  omega1=0.5,omega2=0.5
+  mers_pomp,
+  c1 = 0, c2 = 0,
+  Beta11 = 4, Beta12 = 1,
+  Beta21 = 1, Beta22 = 4,
+  gamma1 = 1, gamma2 = 1,
+  b1 = 0, b2 = 0,
+  d1 = 0, d2 = 0,
+  psi1 = 1, psi2 = 1,
+  omega1 = 0.5, omega2 = 0.5,
+  S1_0 = 300, I1_0 = 10, R1_0 = 10,
+  S2_0 = 300, I2_0 = 10, R2_0 = 10
 )
+
+# Modify the tree to include root not at time 0, have some time before !!! IMPORTANT
+# ballpark 270 infections - > more than model expects 
+# Exhausting S pool -> Increase to be a lot larger  ~ 300 each or so
+
+# Can get ballpark estimate for parameters 
+# Work out number of infections you see -> adjust to have a similar amount in the parameters. i.e r_0 = 4, 
+
+# Suppose for this data -> Case detected 
+# Try increasing I
 
 
 twospecies_params <- data.frame(
-  c1=0.5,c2=0.5,
-  Beta11=4,Beta12=1,
-  Beta21=1,Beta22=4,
-  gamma1=1,gamma2=1,
-  psi1=1,psi2=1,
-  S1_0=100,I1_0=2,R1_0=10,
-  S2_0=100,I2_0=2,R2_0=10,
-  b1=0.1,b2=0.1,
-  d1=0.1,d2=0.1,
-  omega1=0.5,omega2=0.5,
+  c1 = 0, c2 = 0,
+  Beta11 = 4, Beta12 = 1,
+  Beta21 = 1, Beta22 = 4,
+  gamma1 = 1, gamma2 = 1,
+  b1 = 0, b2 = 0,
+  d1 = 0, d2 = 0,
+  psi1 = 1, psi2 = 1,
+  omega1 = 0.5, omega2 = 0.5,
+  S1_0 = 300, I1_0 = 10, R1_0 = 10,
+  S2_0 = 300, I2_0 = 10, R2_0 = 10,
   t0 = 0,
-  time = 5 # max time seen in the tree
+  time = 5.6512919 # max time seen in the tree
 )
 
 
@@ -139,6 +166,7 @@ pfs_mif2 <- readRDS(file = "pfs_results.rds")
 pfs_particle <- readRDS(file = "pfilterList_object.rds")
 
 plot(pfs_particle)
+
 
 
 
